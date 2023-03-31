dc0e4c32c91b20733b3bc07158f9892a72028feb
seppe-vandenberk <seppe.vandenberk@student.uhasselt.be>
2023-03-31 10:59:38 +0200

    first commit

---
+++ app/src/main/java/be/seppevandenberk/degrootrally/MainActivity.kt
@@ -0,0 +1,11 @@
+package be.seppevandenberk.degrootrally
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
