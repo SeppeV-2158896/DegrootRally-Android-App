a5345f46b00408e280a8f99ea1d58e6594873dfa
seppe-vandenberk <seppe.vandenberk@student.uhasselt.be>
2023-05-13 23:33:27 +0200

    Remove Errors and warnings, start recyclerview

--- app/src/main/java/be/seppevandenberk/degrootrally/HoofdMenuFragment.kt
+++ app/src/main/java/be/seppevandenberk/degrootrally/HoofdMenuFragment.kt
@@ -1,6 +1,10 @@
 package be.seppevandenberk.degrootrally
 
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import be.seppevandenberk.degrootrally.databinding.FragmentHoofdMenuBinding
 
@@ -10,5 +14,25 @@ class HoofdMenuFragment : Fragment() {
     //_binding is nullable door dat fragments aangemaakt en verwijdert moeten worden
     private var _binding: FragmentHoofdMenuBinding? = null
     private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHoofdMenuBinding.inflate(inflater,container,false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 
 }
--- app/src/main/java/be/seppevandenberk/degrootrally/MainActivity.kt
+++ app/src/main/java/be/seppevandenberk/degrootrally/MainActivity.kt
@@ -2,10 +2,14 @@ package be.seppevandenberk.degrootrally
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import be.seppevandenberk.degrootrally.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
 }
