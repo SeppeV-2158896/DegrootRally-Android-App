0a18e438ba5c128cbe97f3597ad132fa64896ec8
seppe-vandenberk <seppe.vandenberk@student.uhasselt.be>
2023-05-14 13:54:06 +0200

    Add menuDrawer

--- app/src/main/java/be/seppevandenberk/degrootrally/MainActivity.kt
+++ app/src/main/java/be/seppevandenberk/degrootrally/MainActivity.kt
@@ -2,14 +2,43 @@ package be.seppevandenberk.degrootrally
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import be.seppevandenberk.degrootrally.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+    private var kalenderFragment = KalenderFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setupKalenderFragment()
+        setupActionBarDrawer()
         setContentView(binding.root)
+    }
+    private fun setupKalenderFragment(){
+        supportFragmentManager.beginTransaction().apply{
+            replace(R.id.rally_container,kalenderFragment)
+            commit()
+        }
+    }
+    private fun setupActionBarDrawer(){
+        actionBarDrawerToggle = ActionBarDrawerToggle(this,binding.drawerLayout,R.string.nav_open,R.string.nav_close)
+        binding.drawerLayout.addDrawerListener(actionBarDrawerToggle)
+
+        actionBarDrawerToggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // we need to do this to respond correctly to clicks on menu items, otherwise it won't be caught
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
---
+++ app/src/main/java/be/seppevandenberk/degrootrally/RallyAdapter.kt
@@ -0,0 +1,39 @@
+package be.seppevandenberk.degrootrally
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class RallyAdapter(val items : List<RallyItem>) : RecyclerView.Adapter<RallyAdapter.RallyItemViewHolder>() {
+    inner class RallyItemViewHolder(currentItemView: View) :
+        RecyclerView.ViewHolder(currentItemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RallyItemViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.rally_item_view, parent, false)
+        return RallyItemViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RallyItemViewHolder, position: Int) {
+        val currentRally = items[position]
+        holder.itemView.apply {
+            findViewById<TextView>(R.id.TitelRallyEnDatum).text =
+                currentRally.title.plus(" - ").plus(currentRally.date.toString())
+            if (currentRally.result != null) {
+                findViewById<TextView>(R.id.Resultaat).text = currentRally.result.toString()
+            } else {
+                findViewById<TextView>(R.id.Resultaat).text = " "
+            }
+            findViewById<TextView>(R.id.PilootEnCoPiloot).text =
+                currentRally.piloot.plus(" - ").plus(currentRally.copiloot)
+
+        }
+    }
+
+    override fun getItemCount(): Int = items.size
+
+
+
+}
